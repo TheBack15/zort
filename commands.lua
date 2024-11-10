a4ba9dfc16ffcbdf07792931e81a99dcd461d094
TheBack15 <gravitybacl55@gmail.com>
2024-11-10 15:24:05 +0300

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -5,8 +5,35 @@ local p = game:GetService("Players").LocalPlayer
 local tweenService = game:GetService("TweenService")
 local join = game:GetService("Players").PlayerAdded
 local leave = game:GetService("Players").PlayerRemoving
+local screen = Instance.new("ScreenGui", p.PlayerGui)
+local frame = Instance.new("Frame", screen)
+local button = Instance.new("TextButton", frame)
+local drag = Instance.new("UIDragDetector", frame)
+frame.Position = UDim2.fromScale(0.83,0.748)
+frame.Size = UDim2.fromScale(0.037,0.07)
+frame.Transparency = 0.75
+frame.BackgroundColor3 = Color3.fromRGB(0,0,0)
+button.Position = UDim2.fromScale(0.5, 0.5)
+button.AnchorPoint = Vector2.new(0.5, 0.5)
+button.Size = UDim2.fromScale(0.75, 0.75)
+button.BackgroundColor3 = Color3.fromRGB(127.5,0,0)
+button.BackgroundTransparency = 0.75
+button.Text = "DESTROY"
+button.MouseButton1Click:Connect(function()
+	for _, v in ipairs(activeBillboards[p.Name]) do
+		v:Destroy()
+	end	
+	activeBillboards = nil
+	p = nil
+	join = nil
+	leave = nil
+	screen:Destroy()
+	tweenService:Destroy()
+	highest = nil
+end)
 if join then
 	join:Connect(function(player)
+		timeSpent = os.clock()
 		local bill = Instance.new("BillboardGui", p.Character.Head)
 		local text = Instance.new("TextLabel", bill)
 		local text2 = Instance.new("TextLabel", bill)
@@ -20,7 +47,7 @@ if join then
 		bill.AlwaysOnTop = true
 		bill.Adornee = p.Character.Head
 		bill.StudsOffset = Vector3.new(0,1.5 + offsetCount,0)
-		
+
 		image.BackgroundTransparency = 1
 		image.Position = UDim2.fromScale(0.105,0.5)
 		image.AnchorPoint = Vector2.new(0.5,0.5)
@@ -44,7 +71,7 @@ if join then
 		local gradient2 = Instance.new("UIGradient", text2)
 		gradient2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.fromRGB(108, 255, 108)), ColorSequenceKeypoint.new(0.3, Color3.fromRGB(108, 255, 108)), ColorSequenceKeypoint.new(1, Color3.fromRGB(0,0,0))}
 		gradient2.Rotation = 90
-		
+
 		Instance.new("UIStroke", text3).Thickness = 3
 		local gradient3 = Instance.new("UIGradient", text3)
 		gradient3.Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.fromRGB(108, 255, 108)), ColorSequenceKeypoint.new(0.3, Color3.fromRGB(108, 255, 108)), ColorSequenceKeypoint.new(1, Color3.fromRGB(0,0,0))}
@@ -66,8 +93,8 @@ if join then
 		text2.TextColor3 = Color3.fromRGB(255,255,255)
 		text2.TextScaled = true
 		text2.Text = player.DisplayName .. " (" .. "@" .. player.Name .. ")"
-		
-		
+
+
 		text3.BackgroundTransparency = 1
 		text3.Position = UDim2.fromScale(0,0)
 		text3.Size = UDim2.fromScale(1,0.125)
@@ -85,11 +112,21 @@ if join then
 		local accountAge = player.AccountAge
 		local ageString = daysToYMD(accountAge)
 		text3.Text = ageString
-		
+
 		local rand = 100 / (math.random()*100)
+		if rand >= 1000 and rand < 10000 then
+			local audio = Instance.new("Sound", workspace)
+			audio.SoundId = "rbxassetid://6783209805"
+			audio:Play()
+			audio.Ended:Connect(function()audio:Destroy()end)
+		elseif rand >= 10000 then
+			local audio = Instance.new("Sound", workspace)
+			audio.SoundId = "rbxassetid://243702801"
+			audio:Play()
+			audio.Ended:Connect(function()audio:Destroy()end)
+		end
 		rand = string.format("%.3f", rand)
 		text.Text = "1 in " .. rand
-
 		local tweenStart = TweenInfo.new(3,Enum.EasingStyle.Elastic,Enum.EasingDirection.Out)
 		local properties1 = {Size = UDim2.fromScale(6,1.25)}
 		tweenService:Create(bill,tweenStart, properties1):Play()
@@ -112,6 +149,7 @@ if leave then
 		local text2 = Instance.new("TextLabel", bill)
 		local image = Instance.new("ImageLabel", bill)
 		local text3 = Instance.new("TextLabel", image)
+		local text4 = Instance.new("TextLabel", image)
 		activeBillboards[p.Name] = activeBillboards[p.Name] or {}
 
 		local offsetCount = #activeBillboards[p.Name]
@@ -146,12 +184,18 @@ if leave then
 		local gradient2 = Instance.new("UIGradient", text2)
 		gradient2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 108, 108)), ColorSequenceKeypoint.new(0.3, Color3.fromRGB(255, 108, 108)), ColorSequenceKeypoint.new(1, Color3.fromRGB(0,0,0))}
 		gradient2.Rotation = 90
-		
+
 		Instance.new("UIStroke", text3).Thickness = 2
-		
+
 		local gradient3 = Instance.new("UIGradient", text3)
 		gradient3.Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 108, 108)), ColorSequenceKeypoint.new(0.3, Color3.fromRGB(255, 108, 108)), ColorSequenceKeypoint.new(1, Color3.fromRGB(0,0,0))}
 		gradient3.Rotation = 90
+
+		Instance.new("UIStroke", text4).Thickness = 2
+
+		local gradient4 = Instance.new("UIGradient", text4)
+		gradient4.Color = ColorSequence.new{ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 108, 108)), ColorSequenceKeypoint.new(0.3, Color3.fromRGB(255, 108, 108)), ColorSequenceKeypoint.new(1, Color3.fromRGB(0,0,0))}
+		gradient4.Rotation = 90
 
 		text.BackgroundTransparency = 1
 		text.AnchorPoint = Vector2.new(1,1)
@@ -169,7 +213,7 @@ if leave then
 		text2.TextColor3 = Color3.fromRGB(255,255,255)
 		text2.TextScaled = true
 		text2.Text = player.DisplayName .. " (" .. "@" .. player.Name .. ")"
-		
+
 		text3.BackgroundTransparency = 1
 		text3.Position = UDim2.fromScale(0,0)
 		text3.Size = UDim2.fromScale(1,0.125)
@@ -188,7 +232,32 @@ if leave then
 		local ageString = daysToYMD(accountAge)
 		text3.Text = ageString
 
+		text4.BackgroundTransparency = 1
+		text4.Position = UDim2.fromScale(0,1)
+		text4.AnchorPoint = Vector2.new(0,1)
+		text4.Size = UDim2.fromScale(1,0.125)
+		text4.Font = Enum.Font.Arimo
+		text4.TextColor3 = Color3.fromRGB(255,255,255)
+		text4.TextScaled = true
+
+		if timeSpent then
+			local seconds = os.clock() - timeSpent
+		else
+			text4.Text = "This player was joined to the server before this user did."
+		end
+
 		local rand = 100 / (math.random()*100)
+		if rand >= 1000 and rand < 10000 then
+			local audio = Instance.new("Sound", workspace)
+			audio.SoundId = "rbxassetid://6783209805"
+			audio:Play()
+			audio.Ended:Connect(function()audio:Destroy()end)
+		elseif rand >= 10000 then
+			local audio = Instance.new("Sound", workspace)
+			audio.SoundId = "rbxassetid://243702801"
+			audio:Play()
+			audio.Ended:Connect(function()audio:Destroy()end)
+		end
 		rand = string.format("%.3f", rand)
 		text.Text = "1 in " .. rand
 
